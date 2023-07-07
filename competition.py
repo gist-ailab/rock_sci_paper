cb7bc39adcc2043e6f1c7a1ab72dc61061b98f45
mhko1998 <mhko1998@gist.ac.kr>
2023-07-07 23:50:09 +0900

    commit

--- competition.py
+++ competition.py
@@ -4,13 +4,21 @@ import torch.nn
 import model
 import os
 import time
+import torchvision.transforms as transforms
 
-# save_path = '/home/ailab/Workspace/minhwan/rock_sci_paper/model_para'
-save_path = 'C:\\Users\\USER\\Desktop\\GSH_CRP\\codes\\rock_sci_paper\\model_para'
+save_path = '/home/ailab/Workspace/minhwan/rock_sci_paper/model_para'
+# save_path = 'C:\\Users\\USER\\Desktop\\GSH_CRP\\codes\\rock_sci_paper\\model_para'
 rsp_model = model.ResNet18(num_classes=3)
 rsp_model.load_state_dict(torch.load(os.path.join(save_path, f'teacher.pth')))
 rsp_model = rsp_model.cuda()
 label_dict = {0:'rock', 1:'scissors', 2:'paper'}
+
+transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize((256,256)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))]
+)
 
 webcam = cv2.VideoCapture(0)
 while webcam.isOpened():
@@ -27,8 +35,12 @@ while webcam.isOpened():
         break
 
 img = cv2.imread(os.path.join(save_path, 'inference.jpg'))
-img = cv2.resize(frame, (256,256))
-img = torch.Tensor(img).unsqueeze(0).permute((0,3,1,2)).cuda()
+# img = cv2.imread('/home/ailab/Workspace/minhwan/rock_sci_paper/data/ro_sci_pa_heo/rock/13.jpg')
+
+img = transform(img)
+img = img.unsqueeze(0).cuda()
+
+rsp_model.eval()
 output, _, _, _, _ = rsp_model(img)
 _, pred = torch.max(output, 1)
 label = label_dict[pred.item()]
