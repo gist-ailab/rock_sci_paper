c5046d9c8a70d1c77b17b5265d4e5832017a8747
Yunjae101 <boss9911@naver.com>
2023-07-08 22:29:18 +0900

    commit

--- utils/trainValTestGen.py
+++ utils/trainValTestGen.py
@@ -15,7 +15,7 @@ te_rate = 1-tr_rate-va_rate
 np.random.seed(seed)
 random.seed(seed)
 
-save_path = f'C:\\Users\\USER\\Desktop\\GSH_CRP\\codes\\rock_sci_paper\\data\\ro_sci_pa_heo\\'
+save_path = f'C:\\Users\\USER\\Desktop\\GSH_CRP\\codes\\rock_sci_paper\\data\\pic128'
 
 tr_f = open(os.path.join(save_path, 'train.txt'), 'w')
 va_f = open(os.path.join(save_path, 'val.txt'), 'w')
@@ -26,7 +26,7 @@ total_names = []
 gestures = ['paper', 'rock', 'scissors']
 for gesture in gestures:
     # gesture = 'scissor'
-    d_path = f'C:\\Users\\USER\\Desktop\\GSH_CRP\\codes\\rock_sci_paper\\data\\ro_sci_pa_heo\\{gesture}\\*'
+    d_path = f'C:\\Users\\USER\\Desktop\\GSH_CRP\\codes\\rock_sci_paper\\data\\pic128\\{gesture}\\*'
 
     img_paths = glob(d_path, recursive=True)
     # print(img_paths)
