e85f5394dd60363cbb84028c953edb3e202fb2be
Yunjae101 <boss9911@naver.com>
2023-07-09 22:04:23 +0900

    commit

--- dataset.py
+++ dataset.py
@@ -1,18 +1,11 @@
 import os
-import torch
 from torch.utils.data import Dataset
-import glob
 import cv2
-from PIL import Image
-import numpy as np
 
 class RockScissorsPaper(Dataset):
     def __init__(self, transform=None, path='./DATA', mode='train'):
-        self.class_num = 3
-        self.mode = mode
         self.transform = transform
         self.rootpath = path
-        self.img_path = glob.glob(os.path.join(path,'paper/*'))+glob.glob(os.path.join(path,'rock/*'))+glob.glob(os.path.join(path,'scissors/*'))
         self.label_dict = {'rock':0, 'scissors':1, 'paper':2}
         self.names = []
         with open(os.path.join(self.rootpath, f'{mode}.txt'), 'r') as f:
