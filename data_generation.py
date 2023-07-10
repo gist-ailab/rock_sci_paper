9862075fdc1322b4c635f84d6de8d5c56c91695a
mhko1998@gist.ac.kr <mhko1998@gm.gist.ac.kr>
2023-07-11 07:42:11 +0900

    minhwan_photo

--- data_generation.py
+++ data_generation.py
@@ -8,16 +8,17 @@ webcam.set(cv2.CAP_PROP_FRAME_WIDTH, 128)
 webcam.set(cv2.CAP_PROP_FRAME_HEIGHT, 128)
 
 parser = argparse.ArgumentParser(description='rock-scissor-paper-dataset')
-parser.add_argument('--people', type=str, default = "kominhwan")
 parser.add_argument('--data_num', type=int, default=50)
 args = parser.parse_args()
 
 i=0
 data_num = args.data_num
-people = args.people
+
 
 #data_path = "C:\\Users\\minhwan\\rock_sci_paper\\data"
-data_path = "C:\\Users\\gs23\\Source\\Repos\\rock_sci_paper\\data"
+data_path = "C:\\Users\\gs10\\source\\repos\\rock_sci_paper\\data"
+people = "kominhwan"
+class_name = "Tue"
 arr = []
 
 for k in range(data_num):
@@ -29,14 +30,14 @@ if not webcam.isOpened():
     print("Could not open webcam")
     exit()
 
-if not os.path.isdir(data_path+"\\rock_scissor_paper"):
-    os.mkdir(data_path+"\\rock_scissor_paper")
-if not os.path.isdir(data_path+"\\rock_scissor_paper\\rock"):
-    os.mkdir(data_path+"\\rock_scissor_paper\\rock")
-if not os.path.isdir(data_path+"\\rock_scissor_paper\\scissors"):
-    os.mkdir(data_path+"\\rock_scissor_paper\\scissors")
-if not os.path.isdir(data_path+"\\rock_scissor_paper\\paper"):
-    os.mkdir(data_path+"\\rock_scissor_paper\\paper")
+if not os.path.isdir(data_path+"\\"+class_name):
+    os.mkdir(data_path+"\\"+class_name)
+if not os.path.isdir(data_path+"\\"+class_name+"\\rock"):
+    os.mkdir(data_path+"\\"+class_name+"\\rock")
+if not os.path.isdir(data_path+"\\"+class_name+"\\scissors"):
+    os.mkdir(data_path+"\\"+class_name+"\\scissors")
+if not os.path.isdir(data_path+"\\"+class_name+"\\paper"):
+    os.mkdir(data_path+"\\"+class_name+"\\paper")
 
 while webcam.isOpened():
     status, frame = webcam.read()
@@ -48,13 +49,12 @@ while webcam.isOpened():
     
     ### p 사진 찍기
     if key == ord('p'):
-        if os.path.isfile(data_path+"\\rock_scissor_paper\\"+str(arr[i].split("_")[0])+"\\"+people+str(int(arr[i].split("_")[1]))+".jpg"):
+        if os.path.isfile(data_path+"\\"+class_name+"\\"+str(arr[i].split("_")[0])+"\\"+people+str(int(arr[i].split("_")[1]))+".jpg"):
             print("Wrong people num!")
             break
         else:
-            cv2.imwrite(data_path+"\\rock_scissor_paper\\"+str(arr[i].split("_")[0])+"\\"+people+str(int(arr[i].split("_")[1]))+".jpg", frame)
-            print(data_path+"\\rock_scissor_paper\\"+str(arr[i].split("_")[0])+"\\"+people+str(int(arr[i].split("_")[1]))+".jpg")
-            
+            cv2.imwrite(data_path+"\\"+class_name+"\\"+str(arr[i].split("_")[0])+"\\"+people+str(int(arr[i].split("_")[1]))+".jpg", frame)
+            print(data_path+"\\"+class_name+"\\"+str(arr[i].split("_")[0])+"\\"+people+str(int(arr[i].split("_")[1]))+".jpg")
             
             i+=1
             cv2.destroyAllWindows()
@@ -62,7 +62,6 @@ while webcam.isOpened():
     ### 잘못 찍었으면 이전 데이터 삭제하고 다시 시작
     if key == ord('q'):
         i-=1
-        os.remove(data_path+"\\rock_scissor_paper\\"+str(arr[i].split("_")[0])+"\\"+people+str(int(arr[i].split("_")[1]))+".jpg")
         cv2.destroyAllWindows()
         
     if i == len(arr):
